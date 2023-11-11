f3d3f6729a016d8fd2219d1ddfbcb484ab1d7a43
Darren Tyson - iMP <darren.tyson@vanderbilt.edu>
2023-11-11 11:29:52 -0600

    Updating makeFileInfo function to name saved file appropriately.

--- R/MXprocessing.r
+++ R/MXprocessing.r
@@ -2,10 +2,10 @@ getPlateInfo <- function(MBKfilePath)
 {
     #' Extract file annotation from MBK file exported from MetaXpress
     #'
-    #' Function to extract ImageXpress image file annotations from files exported  
+    #' Function to extract ImageXpress image file annotations from files exported
     #'  MetaXpress server software (Molecular Devices)
     #' @param MBKfilePath character or path to file
-    #' 
+    #'
     #' @return data.frame of file annotations.
 
     message(cat("\nProcessing:",MBKfilePath,"\n"))
@@ -21,7 +21,7 @@ getPlateInfo <- function(MBKfilePath)
     )
     pid <- gsub('Plate','',basename(MBKfilePath))
     d$well <- fixWellName(paste0(LETTERS[d$WELL_Y],d$WELL_X))
-    
+
     # if z-stack, capture z positions
     if(any(d$Z_INDEX > 0))
     {
@@ -35,7 +35,7 @@ getPlateInfo <- function(MBKfilePath)
 
     # expecting 6 columns of data
     pinames <- c('expt_class','expt_id','plate_name','date','plate_id','timepoint')
-    
+
     colnames(plate.info) <- pinames
 
     temp <- d$T_POSITION
@@ -44,7 +44,7 @@ getPlateInfo <- function(MBKfilePath)
     if(ZSTACK) d$z_pos <- z_pos
 
     d$time <- strptime(temp, format='%Y-%m-%d %H:%M:%S')
-    
+
     return(cbind(d,plate.info))
 }
 
@@ -53,7 +53,7 @@ findPlateDir <- function(dirpath)
     #' Recursively search for directories containing "Plate" in their names
     #'
     #' @param dirpath character or path to top directory to search
-    #' 
+    #'
     #' @return character of directory paths.
 
     # find directories in top directory
@@ -65,25 +65,26 @@ findPlateDir <- function(dirpath)
         message(paste('Could not find any directories in',dirpath))
         return(NA)
     }
-    
-    if(!any(grepl('Plate',dl))) 
+
+    if(!any(grepl('Plate',dl)))
     {
         # check in subdirectories
         subdir <- sapply(dl, function(x) list.dirs(x,recursive=FALSE))
         dl <- append(dl,subdir)
     }
-    
+
     # must have 'Plate' in directory name
     return(dl[grepl('Plate',dl)])
 }
 
 
 makeFileInfo <- function(topdir, save=TRUE, overwrite=FALSE) {
+    expt_name <- basename(topdir)
     plate_dir <- findPlateDir(topdir)
     fi <- do.call(rbind, lapply(plate_dir, getPlateInfo))
     fi <- fi[!duplicated(fi$file_name),]    # remove any image file duplicates
-    
-    fi_filepath <- file.path(topdir,"ImageFileInfo.csv")
+
+    fi_filepath <- file.path(topdir,paste0(expt_name,"ImageFileInfo.csv"))
     if(save & (!file.exists(fi_filepath) | overwrite)) {
         message(cat("Writing file:",fi_filepath,"\n"))
         write.csv(fi, file=fi_filepath, row.names=FALSE)
